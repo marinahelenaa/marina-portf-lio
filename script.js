c9b27857020665a86c67a4fe9e5870026c30292d
marinahelenaa <hmarina1905@gmail.com>
2025-05-22 10:53:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleBtn = document.getElementById('mode-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  toggleBtn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+});
